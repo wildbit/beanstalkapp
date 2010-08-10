2a5627ee6edf050087bc62865fadd80d4ba08a2b
Dima Sabanin <sdmitry@gmail.com>
2010-08-10 17:32:50 +0800

    Fixed for new API

--- lib/beanstalkapp/release.rb
+++ lib/beanstalkapp/release.rb
@@ -3,7 +3,11 @@ module Beanstalk::API
     add_prefix ':repository_id'
     
     def self.find_all_for_repository(repo_id)
-      find :all, :params => { :repository_id => repo_id }
+      raise "This method was removed. To lookup Releases use Release#find_all_for_environment method. Sorry."
+    end
+    
+    def self.find_all_for_environment(environment_id, repo_id)
+      find :all, :params => { :repository_id => repo_id, :environment_id => environment_id }
     end
     
     def self.find_for_repository(id, repo_id)
