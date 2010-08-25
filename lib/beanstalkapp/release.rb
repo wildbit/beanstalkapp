978a1c3c3fcb891732a7678938b5c295e0837fe5
Dima Sabanin <sdmitry@gmail.com>
2010-08-25 13:57:55 +0800

    Added pagination support for Releases API

--- lib/beanstalkapp/release.rb
+++ lib/beanstalkapp/release.rb
@@ -6,8 +6,12 @@ module Beanstalk::API
       raise "This method was removed. To lookup Releases use Release#find_all_for_environment method. Sorry."
     end
     
-    def self.find_all_for_environment(environment_id, repo_id)
-      find :all, :params => { :repository_id => repo_id, :environment_id => environment_id }
+    def self.find_all_for_environment(environment_id, repo_id, page=nil)
+      params = { :repository_id => repo_id, :environment_id => environment_id }
+      if page
+        params[:page] = page
+      end
+      find :all, :params => params
     end
     
     def self.find_for_repository(id, repo_id)
